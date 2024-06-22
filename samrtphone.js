e141261168a363506bfe5f4740cb59e6836031f6
Dev2vinod <vinodsinghfun@gmail.com>
2024-06-22 09:31:31 +0530

    model search baki hai

---
+++ samrtphone.js
@@ -0,0 +1,321 @@
+
+let mobile = {
+    Apple: {
+      iphone_14_pro_max: {
+        name: "iphone 14 pro max",
+        rom: "256GB",
+        ram: "12GB",
+        frontCamera: "12 megapixel",
+        backCamera: "13 megapixel",
+        color: ["white", "black", "golden", "grey"],
+        processor: 'A990',
+        price: 'Rs.599,999',
+        image: 'img/AppleiPhone14Pro-b.jpg',
+        discount: 'Rs.699,999'
+      },
+    },
+    Samsung: {
+      Galaxy_S23_ultra: {
+        name: "Galaxy S23 ultra",
+        rom: "512GB",
+        ram: "16GB",
+        backCamera: "200 megapixel",
+        frontCamera: "50 megapixel",
+        color: ["white", "black", "golden", "grey"],
+        processor: 'Snapdragon S880',
+        price: 'Rs.559,999',
+        image: 'img/SamsungGalaxyS23Ultra-b.jpg',
+        discount: 'Rs.599,999'
+  
+      },
+      Galaxy_A35: {
+        name: "Galaxy A35",
+        rom: "128GB",
+        ram: "8GB",
+        backCamera: "20 megapixel",
+        frontCamera: "15 megapixel",
+        color: ["white", "black", "golden", "grey"],
+        processor: 'Snapdragon 600',
+        price: 'Rs.49,999',
+        image: 'img/SamsungGalaxyA35-b.jpg',
+        discount: 'Rs.59,999'
+  
+      },
+    },
+    Infinix: {
+      Infinix_zero_30: {
+        name: "Infinix zero 30",
+        rom: "256GB",
+        ram: "16GB",
+        frontCamera: "50 megapixel",
+        backCamera: "100 megapixel",
+        color: ["white", "black"],
+        processor: 'helio g99k',
+        price: 'Rs.85,999',
+        image: 'img/InfinixZero305G-b.jpg',
+        discount: 'Rs.99,999'
+      },
+      Infinix_GT20_pro: {
+        name: "Infinix GT20 pro",
+        rom: "256GB",
+        ram: "16GB",
+        frontCamera: "50 megapixel",
+        backCamera: "100 megapixel",
+        color: ["white", "black"],
+        processor: 'helio G99k',
+        price: 'Rs.87,999',
+        image: 'img/InfinixGT20Pro-b.jpg',
+        discount: 'Rs.99,999'
+  
+      },
+    },
+    Realme: {
+      Realme_9_pro: {
+        name: "Realme 9 pro",
+        rom: "128GB",
+        ram: "6GB",
+        frontCamera: "12 megapixel",
+        backCamera: "13 megapixel",
+        color: ["white", "black", "ocean blue"],
+        processor: 'Snapdragon 900k',
+        price: 'Rs.105,999',
+        image: 'img/Realme9pro-b.jpg',
+        discount: 'Rs.109,999'
+  
+      },
+    },
+    Techno: {
+      Camon_30_pro: {
+        name: "Camon 30 pro",
+        rom: "256GB",
+        ram: "16GB",
+        frontCamera: "12 megapixel",
+        backCamera: "45 megapixel",
+        color: ["white", "black", "mighty grey"],
+        processor: 'g99 plus',
+        price: 'Rs.75,999',
+        image: 'img/TecnoCamon30Pro-b.jpg',
+        discount: 'Rs.99,999'
+  
+      },
+    },
+    Oneplus: {
+      Oneplus_nord: {
+        name: "Oneplus Nord",
+        rom: "256GB",
+        ram: "16GB",
+        display: "amoled",
+        frontCamera: "12 megapixel",
+        backCamera: "12 megapixel",
+        color: ["white", "black"],
+        processor: 'Snapdragon 670',
+        price: 'Rs.95,999',
+        image: 'img/OnePlusNord-b.jpg',
+        discount: 'Rs.99,999'
+  
+      },
+    },
+    xiaomi: {
+      Poco_F6: {
+        name: "Poco F6",
+        rom: "256GB",
+        ram: "16GB",
+        frontCamera: "52 megapixel",
+        backCamera: "200 megapixel",
+        color: ["white", "black"],
+        processor: 'Snapdragon 890',
+        price: 'Rs.195,999',
+        image: 'img/XiaomiPocoF6-b.jpg',
+        discount: 'Rs.199,999'
+  
+  
+      },
+    },
+    Itel: {
+      itel_S24: {
+        name: "itel S24 256GB",
+        rom: "256GB",
+        ram: "12GB",
+        frontCamera: "12 megapixel",
+        backCamera: "20 megapixel",
+        color: ["white", "black"],
+        processor: 'Helio k33',
+        price: 'Rs.35,999',
+        image: 'img/itelS24256GB-b.jpg',
+        discount: 'Rs.49,999'
+  
+  
+      },
+    },
+    Vivo: {
+      Vivo_X80: {
+        name: "Vivo X80",
+        rom: "512GB",
+        ram: "16GB",
+        frontCamera: "12 megapixel",
+        backCamera: "12 megapixel",
+        color: ["white", "black"],
+        processor: 'Snapdragon 800',
+        price: 'Rs.125,999',
+        image: 'img/VivoX80-b.jpg',
+        discount: 'Rs.149,999'
+  
+      },
+      Vivo_V30: {
+        name: "Vivo V30 pro",
+        rom: "128GB",
+        ram: "8GB",
+        frontCamera: "20 megapixel",
+        backCamera: "45 megapixel",
+        color: ["white"],
+        processor: 'Snapdragon 700',
+        price: 'Rs.45,999',
+        image: 'img/VivoV30-b.jpg',
+        discount: 'Rs.59,999'
+  
+      },
+      Vivo_V29: {
+        name: "Vivo V29",
+        rom: "128GB",
+        ram: "8GB",
+        frontCamera: "20 megapixel",
+        backCamera: "45 megapixel",
+        color: ["white", 'pink'],
+        processor: 'Snapdragon 700',
+        price: 'Rs.65,999',
+        image: 'img/VivoV29-b.jpg',
+        discount: 'Rs.99,999'
+  
+      },
+    },
+    Oppo: {
+      Reno_6_pro: {
+        name: "Oppo Reno 6 pro",
+        rom: "256GB",
+        ram: "16GB",
+        frontCamera: "20 megapixel",
+        backCamera: "120 megapixel",
+        color: ["white", "black"],
+        processor: 'Snapdragon 600',
+        price: 'Rs.55,999',
+        image: 'img/OppoReno6Pro-b.jpg',
+        discount: 'Rs.69,999'
+  
+      },
+      Reno_11F: {
+        name: "Oppo Reno 11F",
+        rom: "256GB",
+        ram: "12GB",
+        frontCamera: "20 megapixel",
+        backCamera: "120 megapixel",
+        color: ["white", "black"],
+        processor: 'Snapdragon 600',
+        price: 'Rs.55,999',
+        image: 'img/OppoReno11F-b.jpg',
+        discount: 'Rs.60,999'
+  
+      },
+    },
+  };
+
+   var selectCategory =document.getElementById("selectCategory")
+   var mobileCard =document.getElementById("mobileCard")
+
+   selectCategory.innerHTML =` <option value="">Select Category</option>`
+   
+  function renderCard(){
+    
+
+     for(var category in mobile){
+        // console.log(category,"i am category")
+        selectCategory.innerHTML +=` <option value="${category}">${category}</option>`
+
+         
+
+        for(var model in mobile[category]){
+            // console.log(model,"model of a phone")
+
+            // console.log(mobile[category][model])
+
+            let data =mobile[category][model]
+
+            let card = `
+        <div class="card bg-base-100 shadow-xl w-[30%] h-[30%] p-3 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  m-2 hover:bg-purple-400 hover:scale-105 rounded-lg">
+            <figure><img src="${data.image ||'./swift.webp'}" alt="Album" class ="w-[50%]" /></figure>
+            <div class="card-body">
+              <h2 class="card-title">Name :${data.name}</h2>
+              <h3>Ram :${data.ram}</h3>
+              <h3>Rom :${data.rom}</h3>
+              <p>Processor :${data.processor}</p>
+              <p>Price:${data.price}</p>
+              <p>Discount : ${data.discount}</p>
+              <div class="card-actions justify-end">
+                <button class="btn btn-primary">Back Camera :${data.backCamera}</button>
+              </div>
+            </div>
+          </div>`
+
+            mobileCard.innerHTML +=card
+            
+        }
+     }
+  }
+
+   renderCard()
+
+
+   var modelName =document.getElementById("modelName")
+
+   modelName.innerHTML =`<option value="">Select Model Name</option>`
+
+    selectCategory.addEventListener("change",()=>{
+      // console.log(selectCategory.value ,"category")
+      // console.log(mobile[selectCategory.value],"apple ka gruop")
+
+
+       // ye nhii chal rah hai ab kya kre hum
+      let data =mobile[selectCategory.value]
+      mobileCard.innerHTML =''
+
+      let card = `
+      <div class="card bg-base-100 shadow-xl w-[30%] h-[30%] p-3 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  m-2 hover:bg-purple-400 hover:scale-105 rounded-lg">
+          <figure><img src="${data.image ||'./swift.webp'}" alt="Album" class ="w-[50%]" /></figure>
+          <div class="card-body">
+            <h2 class="card-title">Name :${data.name}</h2>
+            <h3>Ram :${data.ram}</h3>
+            <h3>Rom :${data.rom}</h3>
+            <p>Processor :${data.processor}</p>
+            <p>Price:${data.price}</p>
+            <p>Discount : ${data.discount}</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Back Camera :${data.backCamera}</button>
+            </div>
+          </div>
+        </div>`
+
+          mobileCard.innerHTML +=card
+        
+
+      // console.log(mobile.selectCategory.value,"single product de rha hai")
+
+      for(var category in mobile){
+        // console.log(category,"loop")
+
+        // console.log(mobile[category])
+
+        for(var model in mobile[category]){
+
+          // console.log(model,"model hu")
+        }
+      }
+
+    })
+
+    // function modelMobile(){
+
+      
+
+
+
+
+    // }
